20091d7476a078ac076ef9f32606ebf3d4e44b76
kmcandersen <kristen.andersen@gmail.com>
2021-12-31 15:33:05 -0600

    change image to show selected variant

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,20 @@
-// Put your application javascript here
+document.addEventListener("DOMContentLoaded", function () {
+
+    let option = document.querySelector(".variant-select");
+   
+    if (option) {
+        option.addEventListener('change', function (event) {
+            let target = event.target.value
+            // get active image (by id of selected option)
+            let productImage = document.querySelector('.single-product-image[data-variant="' + target + '"]')
+            // get all images
+            let productImages = document.querySelectorAll('.single-product-image')
+            //hide all images
+            productImages.forEach(image => {
+                image.style.display = "none"
+            })
+            // show only active image
+            productImage.style.display = 'block'
+        })
+    }
+});
